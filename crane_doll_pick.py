14d1c84361d83806ff67596df912a674a1310bd6
lsw1104 <tomy807@naver.com>
2021-04-05 20:04:38 +0900

    pop하면서 끄집어내기

---
+++ crane_doll_pick.py
@@ -0,0 +1,5 @@
+def solution(board, moves):
+    answer = 0
+    new_board=list((map(list,zip(*mylist)))) #리스트 뒤집기!!
+    
+    return answer
--- test1.py
+++ test1.py
@@ -1,4 +1,12 @@
-li=[0]*5
-print(li)
-    
-    
+import collections
+a=[[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
+b=[[i for i in range(5)]for _ in range(5)]
+b[3][2]=100
+c=[]
+print(b)
+mylist=list(map(list,zip(*a)))
+print(mylist)
+deq=collections.deque(mylist)
+deq.popleft()
+print(deq)
+print(mylist)
