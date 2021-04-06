1a3ec667092881d03747510b947d90d334a1e5d2
leesewoong <44529918+tomy807@users.noreply.github.com>
2021-04-06 18:33:05 +0900

    Update crane_doll_pick.py

--- crane_doll_pick.py
+++ crane_doll_pick.py
@@ -5,15 +5,15 @@ def solution(board, moves):
     collect=[]
     stack=[]
     new_board=list((map(list,zip(*board)))) #리스트 뒤집기!!
-    for i in range(len(new_board)):
+    for i in range(len(new_board)):     #각 줄마다 deque 만들어서 list에 넣기
         deq=collections.deque(new_board[i])
         dqlist.append(deq)
-    for i in moves:
+    for i in moves:     #moves에 따라 deque에서 pop
         while True:
             if dqlist[i-1]:
                 a=dqlist[i-1].popleft()
                 if a!=0:
-                    collect.append(a)
+                    collect.append(a)      #0이 나오면 숫자가 나올때까지 pop, 나온 숫자를 collect에 넣기
                     break
             else:
                 break
