2a4c8276b4cd9aabe4878fb97c77e3be167a1f5a
lsw1104 <tomy807@naver.com>
2021-06-04 12:46:54 +0900

    연속합 백준 1912

--- beakjoon/contisum.py
+++ beakjoon/contisum.py
@@ -1,21 +1,14 @@
 import sys
+
 def solve(n):
-    ans=[]
-    ans.append(dp[0])
+    ans=dp[0]
     for i in range(1,n):
         dp[i]=max(dp[i-1]+li[i],li[i])
-        ans.append(dp[i])
-    return max(ans)
+        ans=max(ans,dp[i])
+    return ans
 if __name__=="__main__":
     n=int(input())
     li=list(map(int,input().split()))
-    dp=[0]*1001
+    dp=[0]*100001
     dp[0]=li[0]
     print(solve(n))
-
-n = int(input())
-a = list(map(int, input().split()))
-sum = [a[0]]
-for i in range(len(a) - 1):
-    sum.append(max(sum[i] + a[i + 1], a[i + 1]))
-print(max(sum))
