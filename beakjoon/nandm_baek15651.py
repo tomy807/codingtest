1f0f1ea05fccd45c06a344bd24af08c1c98ddbbf
lsw1104 <tomy807@naver.com>
2021-08-29 16:33:09 +0900

    n과m 3백준 15651

--- beakjoon/nandm_baek15651.py
+++ beakjoon/nandm_baek15651.py
@@ -2,6 +2,6 @@ import sys
 import itertools
 usingNumbers,lenght = map(int,sys.stdin.readline().split())
 numberlist=[str(i) for i in range(1,usingNumbers+1)]
-results=list(map(' '.join,itertools.combinations_with_replacement(numberlist,repeat=lenght)))
+results=list(map(' '.join,itertools.product(numberlist,repeat=lenght)))
 for result in results:
     print(result)
