abf6f27be542c8d00ad654284c763574c55c9c78
lsw1104 <tomy807@naver.com>
2021-10-24 18:29:44 +0900

    문자열 숫자로 바꾸기

---
+++ programmersKit/numstr_to_int.py
@@ -0,0 +1,10 @@
+def solution(s):
+    table={'zero':0,'one':1,'two':2,'three':3, 'four':4,
+           'five':5,'six':6, 'seven':7, 'eight':8, 'nine':9}
+    for key in table.keys():
+        while key in s:
+            keyLenght=len(key)
+            index=s.find(key)
+            s=s[:index]+str(table[key])+s[index+keyLenght:]
+    return int(s)
+print(solution("oneoneoneone"))
