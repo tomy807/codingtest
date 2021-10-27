bf8696185c317a3239777068cbed9a016dca08f8
lsw1104 <tomy807@naver.com>
2021-10-27 16:12:38 +0900

    날짜 계산

---
+++ programmersKit/dayOfTheWeek.py
@@ -0,0 +1,24 @@
+def solution(a, b):
+    MonthlyDays=[31,29,31,30,31,30,31,31,30,31,30,31]
+    days=0
+    for i in range(0,a-1):
+        days+=MonthlyDays[i]
+    days=days+b-1
+    day=(days)%7
+    if day==0:
+        return "FRI"
+    elif day==1:
+        return "SAT"
+    elif day==2:
+        return "SUN"
+    elif day==3:
+        return "MON"
+    elif day==4:
+        return "TUE"
+    elif day==5:
+        return "WED"
+    else:
+        return "THU"   
+
+print(solution(1,31))
+
