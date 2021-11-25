4d3a14fd85fe2c0577ce72f256683f2a070ef7b9
lsw1104 <tomy807@naver.com>
2021-11-25 14:02:56 +0900

    쿼드압축후개수세기

---
+++ programmersKit/퀴드압축후개수세기.py
@@ -0,0 +1,18 @@
+def solution(arr):
+    answer = [0,0]
+
+    def rec(a,b,n):
+        number=arr[a][b]
+        for i in range(a,a+n):
+            for j in range(b,b+n):
+                if arr[i][j]!=number:
+                    nn=n//2
+                    rec(a, b, nn)
+                    rec(a+nn, b, nn)
+                    rec(a, b+nn, nn)
+                    rec(a+nn, b+nn, nn)
+                    return
+        answer[number]+=1
+    rec(0, 0, len(arr))
+    return answer
+print(solution([[1,1,0,0],[1,0,0,0],[1,0,0,1],[1,1,1,1]]))
