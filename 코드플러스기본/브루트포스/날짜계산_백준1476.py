74f1d3f4e0870e6daea175638b3adc9afb778776
lsw1104 <tomy807@naver.com>
2021-12-24 13:31:14 +0900

    날짜계산-백준1476

---
+++ 코드플러스기본/브루트포스/날짜계산_백준1476.py
@@ -0,0 +1,19 @@
+import sys
+E,S,M = map(int,sys.stdin.readline().split(' '))
+e,s,m=1,1,1
+answer=1
+while(True):
+    if e==E and s==S and m==M:
+        break
+    else:
+        e+=1
+        s+=1
+        m+=1
+        answer+=1
+        if e>=16:
+            e=1
+        if s>=29:
+            s=1
+        if m>=20:
+            m=1
+print(answer)
