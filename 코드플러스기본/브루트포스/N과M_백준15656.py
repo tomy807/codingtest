4ebba51cae2e96f0431791daf131ce51cb65e8ab
lsw1104 <tomy807@naver.com>
2021-12-26 17:13:07 +0900

    N과M 백준15656

---
+++ 코드플러스기본/브루트포스/N과M_백준15656.py
@@ -0,0 +1,17 @@
+N,M= map(int,input().split(' '))
+nums=list(map(int,input().split(' ')))
+nums.sort()
+out=[]
+visited=[False]*N
+def dfs(N,M,depth):
+    if depth==M:
+        print(' '.join(map(str,out)))
+        return
+    else:
+        for i in range(N):
+            if not visited[i]:
+                out.append(nums[i])
+                dfs(N, M, depth+1)
+                out.pop()  
+                visited[i]=False
+dfs(N, M, 0)
