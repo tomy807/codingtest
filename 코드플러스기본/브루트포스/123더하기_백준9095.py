e77d8c37e345591cc83c7d84cce41b3d7aa93d59
lsw1104 <tomy807@naver.com>
2021-12-27 16:04:36 +0900

    123더하기 백준9095 재귀

--- 코드플러스기본/브루트포스/123더하기_백준9095.py
+++ 코드플러스기본/브루트포스/123더하기_백준9095.py
@@ -1,5 +1,4 @@
 T=int(input())
-
 dp=[0]*11
 dp[1]=1
 dp[2]=2
@@ -9,3 +8,16 @@ for i in range(5,11):
     dp[i]=dp[i-1]+dp[i-2]+dp[i-3]
 for _ in range(T):
     print(dp[int(input())])
+# 재귀
+T=int(input())
+def dfs(n):
+    if n==1:
+        return 1
+    if n==2:
+        return 2
+    if n==3:
+        return 4
+    else:
+        return dfs(n-1)+dfs(n-2)+dfs(n-3)
+for _ in range(T):
+    print(dfs(int(input())))
