b82d5911587d8053db17659d90c66414e25dcced
lsw1104 <tomy807@naver.com>
2022-05-12 15:12:10 +0900

    수정렬하기3_백준10989

---
+++ beakjoon/수정렬하기3_백준10989.py
@@ -0,0 +1,9 @@
+import sys
+n = int(sys.stdin.readline())
+l = [0]*10001
+for _ in range(n):
+    l[int(sys.stdin.readline())]+=1
+for i in range(10001):
+    if l[i] !=0:
+        for j in range(l[i]):
+            print(i)
